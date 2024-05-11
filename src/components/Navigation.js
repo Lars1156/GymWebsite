670bb0d6b13e115dd4e321b40d9b2914dbf20662
Optimus115 <91112019+Optimus115@users.noreply.github.com>
2024-05-11 08:25:55 +0530

    changes in naavigation bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {Link} from 'react-router-dom';
-const Navigation = ()=>{
-    return(
-        <nav className = 'navigation'>
-            
-        </nav>
-    )
-}
 
-export default Navigation;
+
+export default function NAvbar(){
+  const [click , setClick] = useState(false);
+
+  function handleClick(){
+    setClick(click);
+  }
+  function closeMobileMenu(){
+    setClick(false);
+  }
+  return(
+     <>
+       <nav className='navbar'>
+        <div className='navbar-container'>
+          <ul className= {click ?'nav-menu-active': 'nav-menu'}>
+              <li className='nav-item'>
+                <a href='/' className='nav-links' onClick={closeMobileMenu}>Home</a>
+              </li>
+              <li className='nav-item'>
+                <a href='/Features' className='nav-links' onClick={closeMobileMenu}>Features</a>
+              </li>
+              <li className="nav-item"> <a href="/Plans" className="nav-links" onClick={closeMobileMenu}> Plans </a></li>
+             <li className="nav-item"> <a href="/Trainers" className="nav-links" onClick={closeMobileMenu}> Trainers </a></li>
+          </ul>
+        </div>
+       </nav>
+     </>
+  )
+}
