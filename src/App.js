cd8f75d67dcce3b3f214c0477a048fdbcfc2b82e
Optimus115 <91112019+Optimus115@users.noreply.github.com>
2024-05-13 10:44:25 +0530

    Buttton component adding

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navigation from './components/Navigation';
 import './App.css';
 import Team from './components/Team';
+import Membership from './components/Membership';
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
       <Team/>
+      <Membership/>
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import React from "react";
+const STYLE =['btn--primary', 'btn--outline', 'btn--secondary'];
+const SIZE =['btn--medium', 'btn--large','btn-small'];
+
+export const Button = ({children, type, onClick , buttonStyle,buttonSize})=>{
+    const checkButtonStyle = STYLE.includes(buttonStyle)? buttonStyle: STYLE[0];
+    const  checkButtonSize = SIZE.includes(buttonSize)? buttonSize: SIZE[0];
+    return(
+        <>
+            <div className="btn-mobile">
+               <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={onClick} type={type} >{children}</button>
+            </div>
+        </>
+    ) 
+}
